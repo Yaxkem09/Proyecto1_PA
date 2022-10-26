ca97c868448b54842a95ff02b98a0dbba1eac479
Rodrigo <rodriept429@gmail.com>
2022-10-26 09:36:09 -0600

    asjhf

--- ColorMania/ColorMania/MyForm.cpp
+++ ColorMania/ColorMania/MyForm.cpp
@@ -4,7 +4,7 @@ using namespace System;
 using namespace System::Windows::Forms;
 
 [STAThreadAttribute]
-int main(array<String^>^ args) {
+int main() {
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
 	ColorMania::MyForm form;
